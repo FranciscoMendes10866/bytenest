db38d31953a81032006ec0c68cf476a37c2d6b15
FranciscoMendes10866 <fmendes_dev@protonmail.ch>
2021-03-17 14:16:45 +0100

    added frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+import { Button } from './styles/Button'
+
+const App: FC = () => (
+  <>
+    <Button>Hello World</Button>
+  </>
+);
+
+export default App;
---
+++ frontend/src/styles/Button.ts
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled'
+
+export const Button = styled.button`
+  background-color: #0C090D;
+  color: #fff;
+  font-size: 18px;
+  padding-left: 30px;
+  padding-right: 30px;
+  padding-top: 15px;
+  padding-bottom: 15px;
+  margin-left: 30px;
+  margin-top: 30px;
+  border-radius: 6px;
+  border: none;
+  letter-spacing: 1.5px;
+`
