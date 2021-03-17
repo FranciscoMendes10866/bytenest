9ac5fa85845452f31e8346acdec6f188f42a3405
FranciscoMendes10866 <fmendes_dev@protonmail.ch>
2021-03-17 17:23:59 +0100

    added js-cookie to verify if the user has the token and created a new file struct

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,27 @@
 import { FC } from "react";
-import { Container } from "@mverissimoo/emotion-grid";
-import { FaMagento } from "react-icons/fa";
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import { get as getCookie } from 'js-cookie'
 
-import {
-  Nav,
-  Items,
-  Item,
-  ItemAnchor,
-  Brand,
-  Actions,
-  Button,
-  ActionAnchor,
-} from "./styles/Navbar";
-import { Button as Primary } from "./styles/Button";
+import { Navbar } from './components/index'
+import { Home } from './pages'
 
-const App: FC = () => (
-  <Container>
-    <Nav>
-      <Brand>
-        <FaMagento />
-      </Brand>
-      <Items>
-        <Item>
-          <ItemAnchor>Home</ItemAnchor>
-        </Item>
-        <Item>
-          <ItemAnchor>About</ItemAnchor>
-        </Item>
-        <Item>
-          <ItemAnchor>Contact</ItemAnchor>
-        </Item>
-      </Items>
-      <Actions>
-        <ActionAnchor>Sign in</ActionAnchor>
-        <Button>
-          <Primary>Sign up</Primary>
-        </Button>
-      </Actions>
-    </Nav>
-  </Container>
-);
+const App: FC = () => {
+  const isLoggedIn = getCookie('access_token')
+  return (
+    <>
+      <Navbar />
+      <Router>
+        <Switch>
+          { !isLoggedIn ? (
+            <>
+              <Route exact path="/" component={Home} />
+              <Redirect to="/" />
+            </>
+          ) : null }
+        </Switch>
+      </Router>
+    </>
+  )
+};
 
 export default App;
